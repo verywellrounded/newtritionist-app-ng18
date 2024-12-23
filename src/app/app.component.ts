38c1797abea9659d0bab92c7753c3dcb4bcdd27f
Balance.Is.Key <reimaginerei@gmail.com>
2024-12-23 14:59:58 -0500

    Initialized angular fire with ang 18 before doing firebase cli init and setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,41 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import {
+  collection,
+  collectionData,
+  DocumentData,
+  Firestore,
+  getDoc,
+} from '@angular/fire/firestore';
 import { RouterOutlet } from '@angular/router';
+import { Observable } from 'rxjs';
+
+interface Item {
+  name: string;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [CommonModule],
+  template: `
+    <ul>
+      @for (item of (item$ | async); track item) {
+      <li>
+        {{ item['name'] }}
+      </li>
+      }
+    </ul>
+  `,
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'newtritionist-app-ng18';
+  item$: Observable<DocumentData[]>;
+  private firestore = inject(Firestore);
+
+  constructor() {
+    const itemCollection = collection(this.firestore, 'test');
+    this.item$ = collectionData<DocumentData>(itemCollection);
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,27 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'newtritionist-app-angular',
+        appId: '1:427778759049:web:85c1f4d118ca80fe81a958',
+        storageBucket: 'newtritionist-app-angular.firebasestorage.app',
+        apiKey: 'AIzaSyApeHp8f3VbMir4QeabWv-tBG8gbPjh6-0',
+        authDomain: 'newtritionist-app-angular.firebaseapp.com',
+        messagingSenderId: '427778759049',
+        measurementId: 'G-9T4ZYPKPDK',
+      })
+    ),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+  ],
 };
